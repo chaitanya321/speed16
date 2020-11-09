02298ec9928622d9e350078bbc595a6df8d07607
Chaitanya <chaitanya.1600@gmail.com>
2020-11-09 08:00:19 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -187,3 +187,11 @@
   });
 
 })(jQuery);
+
+
+jQuery(document).ready(function($) {
+    $("li.dropdown a").click(function(e){
+        $(this).next('ul.dropdown-menu').css("display", "block");
+        e.stopPropagation();
+        });
+});
