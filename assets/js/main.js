4aeed3e54d3f5eadcb3e60c0d6aa639bdd75a244
Chaitanya <chaitanya.1600@gmail.com>
2020-11-09 08:04:37 +0530

    e

--- assets/js/main.js
+++ assets/js/main.js
@@ -187,11 +187,3 @@
   });
 
 })(jQuery);
-
-
-jQuery(document).ready(function($) {
-    $("li.dropdown a").click(function(e){
-        $(this).next('ul.dropdown-menu').css("display", "block");
-        e.stopPropagation();
-        });
-});
